6d66b352f4824a40792f20d7257decb366a34926
koike <tengaiminagi@gmail.com>
2017-10-22 15:03:20 +0900

    Step aside unitychan 提出一回目
    アップ時、大容量によるエラーが発生しましたと警告が出ました。提出できているでしょうか。

---
+++ Assets/item.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class item : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	void OnBecameInvisible() {
+		Debug.Log (gameObject.name);
+		Destroy (gameObject);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
